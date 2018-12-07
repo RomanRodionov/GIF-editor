39f9bbe78513db03f6df55216172e8dd960bb3ec
RomanRodionov <rodionovv22@gmail.com>
2018-12-08 00:35:44 +0300

    added copy, paste, remove, delete, tabwidget and scaling

--- Editor.py
+++ Editor.py
@@ -2,14 +2,46 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt5.QtWidgets import (QListWidget, QListWidgetItem, QComboBox, QMainWindow, QLineEdit,
-                             QLabel, QAction, QFileDialog, QApplication, QPushButton, QInputDialog)
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import (QSlider, QWidget, QTabWidget, QListWidget, QListWidgetItem, QComboBox, QMainWindow,
+                             QLineEdit, QLabel, QAction, QFileDialog, QApplication, QPushButton, QInputDialog,
+                             QVBoxLayout, QSizePolicy)
+from PyQt5.QtGui import QIcon, QMovie
+from PyQt5.QtCore import QByteArray
 from PyQt5.Qt import QSize
 from PyQt5 import QtCore, QtGui
+from PyQt5.QtCore import Qt
 import imageio
-import os
 from random import randint
+
+gifFile = "ex.gif"
+class GifPlayer(QWidget):
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
+
+        self.movie = QMovie('ex.gif', QByteArray(), self)
+        size = self.movie.scaledSize()
+        self.setGeometry(200, 200, size.width(), size.height())
+        self.movie_screen = QLabel()
+        self.movie_screen.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.movie_screen.setAlignment(Qt.AlignCenter)
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self.movie_screen)
+        self.setLayout(main_layout)
+        self.movie.setCacheMode(QMovie.CacheAll)
+        self.movie_screen.setMovie(self.movie)
+        self.movie.start()
+        self.movie.loopCount()
+
+        button = QPushButton('refresh gif', self)
+        button.setToolTip('This is an example button')
+        button.move(10,10)
+        button.clicked.connect(self.change)
+
+    def change(self, name):
+        self.movie = QMovie(name,QByteArray(), self)
+        self.movie_screen.setMovie(self.movie)
+        self.movie.start()
+        print("done")
 
 
 class Editor(QMainWindow):
@@ -24,7 +56,7 @@ class Editor(QMainWindow):
         self.quality = 256
         self.gif_palette = QComboBox(self)
         self.gif_palette.addItems(["256", "128",
-                                   "64", "32", "16"])
+                                   "64", "32", "16", "8", "4", "2"])
 
         self.gif_palette.move(15, 250)
         self.gif_palette.resize(80, 25)
@@ -32,29 +64,46 @@ class Editor(QMainWindow):
         self.gif_palette.activated[str].connect(self.ch_pal)
         self.path_to_add = '/home'
         self.path_to_save = '/home'
+
         addFile = QAction(QIcon('open.png'), 'Add', self)
         addFile.setShortcut('Ctrl+O')
         addFile.setStatusTip('Add frame')
         addFile.triggered.connect(self.add)
+
         self.btn = QPushButton('Save...', self)
         self.btn.resize(80, 25)
         self.btn.move(15, 460)
         self.btn.clicked.connect(self.save)
 
-        self.btn_c = QPushButton('Clean', self)
-        self.btn_c.resize(80, 25)
-        self.btn_c.move(15, 420)
-        self.btn_c.clicked.connect(self.clean)
+        self.btn_cl = QPushButton('Clean', self)
+        self.btn_cl.resize(80, 25)
+        self.btn_cl.move(15, 420)
+        self.btn_cl.clicked.connect(self.clean)
 
         self.btn_a = QPushButton('Add...', self)
         self.btn_a.resize(80, 25)
         self.btn_a.move(15, 380)
         self.btn_a.clicked.connect(self.add)
 
-        self.btn_r = QPushButton('Delete', self)
+        self.btn_d = QPushButton('Delete', self)
+        self.btn_d.resize(80, 25)
+        self.btn_d.move(15, 60)
+        self.btn_d.clicked.connect(self.delete)
+
+        self.btn_r = QPushButton('Remove', self)
         self.btn_r.resize(80, 25)
-        self.btn_r.move(15, 180)
-        self.btn_r.clicked.connect(self.delete)
+        self.btn_r.move(15, 100)
+        self.btn_r.clicked.connect(self.remove)
+
+        self.btn_c = QPushButton('Copy', self)
+        self.btn_c.resize(80, 25)
+        self.btn_c.move(15, 140)
+        self.btn_c.clicked.connect(self.copy)
+
+        self.btn_p = QPushButton('Paste', self)
+        self.btn_p.resize(80, 25)
+        self.btn_p.move(15, 180)
+        self.btn_p.clicked.connect(self.paste)
 
         saveFile = QAction(QIcon('save.png'), 'Save', self)
         saveFile.setShortcut('Ctrl+S')
@@ -80,21 +129,70 @@ class Editor(QMainWindow):
         self.duration_input.resize(80, 25)
         self.duration_input.setText('0.1')
 
-        self.setGeometry(300, 300, 700, 500)
+        self.setGeometry(300, 300, 810, 580)
         self.setWindowTitle('GIF-editor')
+        self.setFixedSize(self.size())
+
+
 
         # работа со списком изображений
         self.lst = QListWidget(self)
         self.lst.move(150, 40)
-        self.lst.resize(300, 450)
+        self.lst.resize(400, 450)
         self.lst.setIconSize(QSize(90, 90))
 
         pal = self.palette()
 
+        self.buffer = []
+
         pal.setColor(QtGui.QPalette.Normal, QtGui.QPalette.Background, QtGui.QColor("#FFEBCD"))
         pal.setColor(QtGui.QPalette.Inactive, QtGui.QPalette.Background, QtGui.QColor("#FFF8E7"))
         self.setPalette(pal)
+
+        #tabwidget
+        _translate = QtCore.QCoreApplication.translate
+        self.tabWidget = QTabWidget(self)
+        self.tabWidget.setGeometry(QtCore.QRect(120, 25, 670, 510))
+        self.tabWidget.addTab(self.lst, "")
+        self.res_display = GifPlayer()
+
+        self.tabWidget.addTab(self.res_display, "")
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.lst), _translate("Form", "Frames"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.res_display), _translate("Form", "Result"))
+
+        self.slider = QSlider(QtCore.Qt.Horizontal, self)
+        self.slider.setFocusPolicy(QtCore.Qt.StrongFocus)
+        self.slider.setTickPosition(QSlider.TicksBothSides)
+        self.slider.setValue(20)
+        self.slider.move(700, 540)
+        self.slider.valueChanged.connect(self.change_slider)
+
         self.show()
+
+    def change_slider(self):
+        value = self.slider.value() * 6
+        self.lst.setIconSize(QSize(value, value))
+
+    def copy(self):
+        listItems = self.lst.selectedItems()
+        if not listItems:
+            return
+        self.buffer = []
+        for item in listItems:
+            self.buffer.append(self.lst.item(self.lst.row(item)).clone())
+
+    def paste(self):
+        try:
+            ch_item = self.lst.selectedItems()[0]
+            if self.buffer:
+                for item in self.buffer:
+                    self.lst.insertItem(self.lst.row(ch_item), item.clone())
+        except Exception as a:
+            print(a)
+
+    def remove(self):
+        self.copy()
+        self.delete()
 
     def get_lst(self):
         items = []
