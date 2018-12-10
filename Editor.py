087129f261abac36dd17a7283aae44cce34ceec8
RomanRodionov <rodionovv22@gmail.com>
2018-12-10 17:53:57 +0300

    changed colors of widgets, icon and background

--- Editor.py
+++ Editor.py
@@ -6,15 +6,15 @@ from PyQt5.QtWidgets import (QSlider, QWidget, QTabWidget, QListWidget, QListWid
                              QLineEdit, QLabel, QAction, QFileDialog, QApplication, QPushButton, QInputDialog,
                              QVBoxLayout, QSizePolicy)
 from PyQt5.QtGui import QIcon, QMovie
-from PyQt5.QtCore import QByteArray
+from PyQt5.QtCore import QByteArray, Qt
 from PyQt5.Qt import QSize
 from PyQt5 import QtCore, QtGui
-from PyQt5.QtCore import Qt
 from PIL import Image
 import imageio
 from random import randint
 
 
+#Код класс GifPlayer, который создает и проигрывает гиф-изображение
 class GifPlayer(QWidget):
     def __init__(self, name, parent=None):
         QWidget.__init__(self, parent)
@@ -39,7 +39,12 @@ class GifPlayer(QWidget):
         self.btn_s.resize(80, 25)
         self.btn_s.move(0, 460)
         self.btn_s.clicked.connect(self.resizeGIF)
-
+        self.btn_s.setStyleSheet("""
+    QPushButton:hover { background-color: red; color: white }
+    QPushButton:!hover { background-color: rgb(105, 105, 105); color: white }
+    QPushButton:pressed { background-color: rgb(205, 92, 92); color: white }
+""")
+    #Этот метод меняет разрешение гиф-анимации в окне result
     def resizeGIF(self):
         try:
             rect = self.geometry()
@@ -59,7 +64,7 @@ class GifPlayer(QWidget):
         except Exception as er:
             print(er)
 
-
+# Код класса оконного приложения
 class Editor(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -76,6 +81,11 @@ class Editor(QMainWindow):
 
         self.gif_palette.move(15, 250)
         self.gif_palette.resize(80, 25)
+        self.gif_palette.setStyleSheet("""
+    QComboBox:hover { background-color: red; color: white }
+    QComboBox:!hover { background-color: rgb(105, 105, 105); color: white }
+    QComboBox:pressed { background-color: rgb(205, 92, 92); color: white }
+""")
 
         self.gif_palette.activated[str].connect(self.ch_pal)
         self.path_to_add = '/home'
@@ -86,6 +96,7 @@ class Editor(QMainWindow):
         addFile.setStatusTip('Add frame')
         addFile.triggered.connect(self.add)
 
+#Кнопки для работы с программой
         self.btn = QPushButton('Save...', self)
         self.btn.resize(80, 25)
         self.btn.move(15, 460)
@@ -121,11 +132,44 @@ class Editor(QMainWindow):
         self.btn_p.move(15, 180)
         self.btn_p.clicked.connect(self.paste)
 
+        for button in [self.btn, self.btn_a, self.btn_c, self.btn_cl, self.btn_d, self.btn_p, self.btn_r, self.btn]:
+            button.setStyleSheet("""
+    QPushButton:hover { background-color: red; color: white }
+    QPushButton:!hover { background-color: rgb(105, 105, 105); color: white }
+    QPushButton:pressed { background-color: rgb(205, 92, 92); color: white }
+""")
+
         saveFile = QAction(QIcon('save.png'), 'Save', self)
         saveFile.setShortcut('Ctrl+S')
         saveFile.setStatusTip('Save GIF')
         saveFile.triggered.connect(self.save)
         menubar = self.menuBar()
+        self.setStyleSheet("""
+                QMenuBar {
+                    background-color: rgb(49,49,49);
+                    color: rgb(255,255,255);
+                    border: 1px solid #000;
+                }
+
+                QMenuBar::item {
+                    background-color: rgb(49,49,49);
+                    color: rgb(255,255,255);
+                }
+
+                QMenuBar::item::selected {
+                    background-color: rgb(30,30,30);
+                }
+
+                QMenu {
+                    background-color: rgb(49,49,49);
+                    color: rgb(255,255,255);
+                    border: 1px solid #000;           
+                }
+
+                QMenu::item::selected {
+                    background-color: rgb(30,30,30);
+                }
+            """)
 
         fileMenu = menubar.addMenu('&Add')
         fileMenu.addAction(addFile)
@@ -135,19 +179,35 @@ class Editor(QMainWindow):
         self.t1 = QLabel(self)
         self.t1.setText("Duration time:")
         self.t1.move(15, 290)
+        self.t1.setStyleSheet("""
+    QLabel { color: white }
+""")
 
         self.t2 = QLabel(self)
         self.t2.setText("Pallet size:")
         self.t2.move(15, 210)
+        self.t2.setStyleSheet("""
+            QLabel { color: white }
+        """)
 
         self.duration_input = QLineEdit(self)
         self.duration_input.move(15, 330)
         self.duration_input.resize(80, 25)
         self.duration_input.setText('0.1')
+        self.duration_input.setStyleSheet("""
+    QLineEdit:hover { background-color: red; color: white }
+    QLineEdit:!hover { background-color: rgb(105, 105, 105); color: white }
+""")
 
-        self.setGeometry(300, 300, 810, 580)
+        self.setGeometry(300, 300, 810, 575)
         self.setWindowTitle('GIF-editor')
         self.setFixedSize(self.size())
+        self.setWindowIcon(QtGui.QIcon('icon.png'))
+        pal = self.palette()
+        pal.setBrush(QtGui.QPalette.Normal, QtGui.QPalette.Background,
+                     QtGui.QBrush(QtGui.QPixmap("bg.png")))
+        self.setPalette(pal)
+        # Ссылка на изображение: https://wallscloud.net/wallpaper/textures/Siniy-Fon/qLj7
 
         # работа со списком изображений
         self.lst = QListWidget(self)
@@ -159,14 +219,15 @@ class Editor(QMainWindow):
 
         self.buffer = []
 
-        pal.setColor(QtGui.QPalette.Normal, QtGui.QPalette.Background, QtGui.QColor("#FFEBCD"))
-        pal.setColor(QtGui.QPalette.Inactive, QtGui.QPalette.Background, QtGui.QColor("#FFF8E7"))
-        self.setPalette(pal)
+        #pal.setColor(QtGui.QPalette.Normal, QtGui.QPalette.Background, QtGui.QColor("#778899"))
+        #pal.setColor(QtGui.QPalette.Inactive, QtGui.QPalette.Background, QtGui.QColor("#778899"))
+        #self.setPalette(pal)
 
         # tabwidget
         _translate = QtCore.QCoreApplication.translate
         self.tabWidget = QTabWidget(self)
         self.tabWidget.setGeometry(QtCore.QRect(120, 25, 670, 510))
+        self.tabWidget.setStyleSheet(" background-color: rgb(233, 233, 233) ")
         self.tabWidget.addTab(self.lst, "")
 
         self.res_display = GifPlayer('ex.gif')
@@ -184,10 +245,12 @@ class Editor(QMainWindow):
 
         self.show()
 
+    # Изменение размера иконок во вкладке Frames
     def change_slider(self):
         value = self.slider.value() * 6
         self.lst.setIconSize(QSize(value, value))
 
+    # Функция сохранения фрэйма в буфер обмена
     def copy(self):
         listItems = self.lst.selectedItems()
         if not listItems:
@@ -196,6 +259,7 @@ class Editor(QMainWindow):
         for item in listItems:
             self.buffer.append(self.lst.item(self.lst.row(item)).clone())
 
+    # Функция вставки фрэйма из буфера обмена
     def paste(self):
         try:
             ch_item = self.lst.selectedItems()[0]
@@ -205,22 +269,27 @@ class Editor(QMainWindow):
         except Exception as a:
             print(a)
 
+    # Функция удаления фрэйма из вкладки Frames и сохранения его в буфер обмена
     def remove(self):
         self.copy()
         self.delete()
 
+    # Получение списка адресов фрэймов из вкладки Frames
     def get_lst(self):
         items = []
         for index in range(self.lst.count()):
             items.append(self.lst.item(index).text())
         return items
 
+    # Изменение количества цветов в палитре гиф-изображения
     def ch_pal(self, text):
         self.quality = int(text)
 
+    # Функция удаления всех фреймов из вкладки Frames
     def clean(self):
         self.lst.clear()
 
+    # Функция удаления фрэйма
     def delete(self):
         listItems = self.lst.selectedItems()
         if not listItems:
@@ -228,6 +297,7 @@ class Editor(QMainWindow):
         for item in listItems:
             self.lst.takeItem(self.lst.row(item))
 
+    # Функция добавления фрэйма во вкладку Frames
     def add(self):
         name = False
         name = QFileDialog.getOpenFileName(self, 'Choose file', self.path_to_add)[0]
@@ -238,6 +308,7 @@ class Editor(QMainWindow):
                 self.lst.addItem(item)
                 self.path_to_add = name
 
+    # Функция создания гиф-файла и сохранения в директории
     def save(self):
         path = False
         path = QFileDialog.getExistingDirectory(self, 'Choose directory', self.path_to_save)
@@ -261,6 +332,7 @@ class Editor(QMainWindow):
             self.tabWidget.addTab(self.res_display, "")
             self.tabWidget.setTabText(self.tabWidget.indexOf(self.lst), _translate("Form", "Frames"))
             self.tabWidget.setTabText(self.tabWidget.indexOf(self.res_display), _translate("Form", "Result"))
+
 
     def work(self, path):
         try:
